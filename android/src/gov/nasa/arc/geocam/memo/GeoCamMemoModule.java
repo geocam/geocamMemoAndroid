58991c6e03b1941681fc855e8d8ac343f52603fe
Patrick Baumann <baumannpat@gmail.com>
2011-03-19 13:41:09 -0700

    Added bind for gson imp

--- android/src/gov/nasa/arc/geocam/memo/GeoCamMemoModule.java
+++ android/src/gov/nasa/arc/geocam/memo/GeoCamMemoModule.java
@@ -1,14 +1,13 @@
 package gov.nasa.arc.geocam.memo;
 
-import gov.nasa.arc.geocam.memo.service.DjangoMemoImplementation;
-import gov.nasa.arc.geocam.memo.service.DjangoMemoInterface;
+import gov.nasa.arc.geocam.memo.service.*;
 import roboguice.config.AbstractAndroidModule;
 
 public class GeoCamMemoModule extends AbstractAndroidModule{
 
 	@Override
 	protected void configure() {
-		// TODO Auto-generated method stub
 		bind(DjangoMemoInterface.class).to(DjangoMemoImplementation.class);
+		bind(AwesomeJsonConverterInterface.class).to(GsonJsonConverterImplementation.class);
 	}
 }
