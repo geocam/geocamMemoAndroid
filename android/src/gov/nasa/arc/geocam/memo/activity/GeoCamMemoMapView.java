838faae9880947af20872b7f0ad1c6ca5c509bfb
avagadia <anooj.vagadia@sv.cmu.edu>
2011-04-14 10:03:21 -0700

    Some cleanup Work.

--- android/src/gov/nasa/arc/geocam/memo/activity/GeoCamMemoMapView.java
+++ android/src/gov/nasa/arc/geocam/memo/activity/GeoCamMemoMapView.java
@@ -16,12 +16,9 @@ import com.google.android.maps.OverlayItem;
 
 import gov.nasa.arc.geocam.memo.R;
 import gov.nasa.arc.geocam.memo.UIUtils;
-import gov.nasa.arc.geocam.memo.bean.GeoCamMemoMessage;
 import gov.nasa.arc.geocam.memo.service.MemoMapOverlay;
 import roboguice.activity.RoboMapActivity;
-import roboguice.inject.InjectView;
-
-import com.google.inject.Inject;;
+import roboguice.inject.InjectView;;
 
 public class GeoCamMemoMapView extends RoboMapActivity{
 
@@ -35,36 +32,37 @@ public class GeoCamMemoMapView extends RoboMapActivity{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_view);
+		
+		//Get the latitude and longitude from the Intent
 		Intent intent = getIntent();
 		double latitude = intent.getDoubleExtra("Latitude", 0.00);
 		double longitude = intent.getDoubleExtra("Longitude", 0.00);
-		
-		LinearLayout linearLayout;
+				
 		mapView.setBuiltInZoomControls(true);
 		mapOverlays = mapView.getOverlays();
+		
 		drawable = this.getResources().getDrawable(R.drawable.map_marker);
 		itemizedOverlay = new MemoMapOverlay(drawable);
-
 		
-		
+		//Create a GeoPoint to signify the geolocation an overlay containing the geopoint
 		GeoPoint point = new GeoPoint((int)(latitude * 1E6), 
 				                      (int)(longitude * 1E6));
 		OverlayItem overlayitem = new OverlayItem(point, "", "");
 		itemizedOverlay.addOverlay(overlayitem);
 		mapOverlays.add(itemizedOverlay);
+		
+		//Get the map controller and animate to the GeoPoint
 		mapController = mapView.getController();
 		mapController.animateTo(point);
 		mapController.setZoom(16);
 	}
 	
-
 	public void onHomeClick(View v) {
 		UIUtils.goHome(this);
 	}
 	
 	@Override
 	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
